ab21771176d6812f826568478d1352944d4757b9
Guilherme Martin <gsilvamartin@gmail.com>
2020-06-22 15:04:28 -0300

    operações socket refatorada

--- src/controller/socket.js
+++ src/controller/socket.js
@@ -4,14 +4,10 @@ socket.on('connect', function(){
     console.log(socket.id);
 });
 
-socket.on('event', function(data){
-    console.log(data);
-});
-
 socket.on('newplayer', function(data){
     toastr.success('Novo jogador!', 'Jogador ' + data.playerId + ' entrou na partida!');
 });
 
 socket.on('disconnect', function(){
-
+    toastr.error('Jogador desconectado!', '');
 });
